0ab3f3702046fda0333355391884fea28902a3f7
Rafael Fernández López <ereslibre@ereslibre.es>
2010-03-11 03:09:45 +0100

    mapea correctamente el cuadradito

--- practica4/frenet.cpp
+++ practica4/frenet.cpp
@@ -11,7 +11,7 @@ QList<PV3f> Frenet::marco(const QList<PV3f> &listaPuntos, GLdouble t, GLdouble b
     const PV3f vb = ((dpc * ddpc) * pow((dpc * ddpc).mod(), -1.0)).normaliza();
     const PV3f vn = (((dpc * ddpc) * dpc) * pow(((dpc * ddpc) * dpc).mod(), -1.0)).normaliza();
     
-    return multMatriz(listaPuntos, vn, vb, vt, t);
+    return multMatriz(listaPuntos, vn, vt, vb, t);
 }
 
 PV3f Frenet::derivada1(GLdouble t, GLdouble b)
@@ -35,7 +35,7 @@ PV3f Frenet::derivada2(GLdouble t)
 QList<PV3f> Frenet::multMatriz(const QList<PV3f> &listaPuntos, const PV3f &n, const PV3f &b, const PV3f &t, GLdouble s)
 {
     QList<PV3f> res;
-    
+
     foreach (const PV3f &p, listaPuntos) {
         res << PV3f(cos(s), sin(s), s / 2.0) + PV3f(n.dot(p), b.dot(p), t.dot(p), PV3f::Vector);
     }
