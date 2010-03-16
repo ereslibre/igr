22d454861d94a66ffa2d52c69c152d07a3da3280
Jorge Olmos Mallol <freefitman@gmail.com>
2010-03-16 22:39:58 +0100

    Añado la clase para pintar el coche, espero que todo vaya bien.

---
+++ practica4/coche.h
@@ -0,0 +1,34 @@
+#ifndef COCHE_H
+#define COCHE_H
+
+#include "pv3f.h"
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+class Coche
+{
+public:
+    Coche();
+    virtual ~Coche();
+
+    void dibuja(GLdouble scale);
+
+private:
+    void dibujaCubo(double x, double y, double z);
+    void dibujaRect(GLdouble x, GLdouble y, GLdouble z,GLdouble scale);
+    PV3f derivada1(GLdouble t) const;
+    PV3f derivada2(GLdouble t) const;
+
+private:
+    GLUquadric *m_dis1;
+    GLUquadric *m_dis2;
+    GLUquadric *m_dis3;
+    GLUquadric *m_dis4;
+    GLUquadric *m_rue1;
+    GLUquadric *m_rue2;
+    GLUquadric *m_rue3;
+    GLUquadric *m_rue4;
+};
+
+#endif
